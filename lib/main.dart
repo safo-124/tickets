2f035b0269a8f912c9eb65c626882a5ed20c98ff
Safo Emmanuel <nanasafo122@gmail.com>
2024-08-18 15:28:50 +0800

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:tickets_app/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,38 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Tickets"),
-        ),
-        body: const Center(
-          child: Text("Ticket Info"),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.blueGrey,
-          unselectedItemColor: const Color(0xFF526400),
-          showSelectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-                label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-                label: "Search"),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
-                label: "Tickets "),
-            BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
-                label: "Profile"),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(home: BottomNavBar());
   }
 }
